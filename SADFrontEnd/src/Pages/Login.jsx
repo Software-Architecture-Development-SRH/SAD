e3218b427716102d19c40488d27f53ab08130f38
Ankush Gangal <arnavgupta384@gmail.com>
2024-01-19 22:58:05 +0100

    4th Coommit
    Made the Login page along side implementing styling from wrapper

--- SADFrontEnd/src/Pages/Login.jsx
+++ SADFrontEnd/src/Pages/Login.jsx
@@ -1,5 +1,27 @@
+import Logo from "../components/Logo";
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/styles/registerandsignup";
+import FormRow from '../components/FormRow/'
+
 const Login = () => {
-  return <h1>ankush</h1>;
+  return (
+<Wrapper>
+<form className="form">
+  <Logo/>
+  <h4>Login </h4>
+  <FormRow type= 'email' name ='Email' defaultvalue='ankush@gmail.com'/>
+  <FormRow type= 'password' name ='Password' defaultvalue='secret123'/>
+  <button type='submit' className='btn btn-block'>Submit</button>
+  <button type='button' className='btn btn-block'>Expöore without Login</button>
+  <p>
+        Not a member yet?
+        <Link to='/register' className="member-btn">Register</Link>
+      </p>
+      </form>
+</Wrapper>
+
+  );
+
 };
 
 export default Login;
--- SADFrontEnd/src/components/index.js
+++ SADFrontEnd/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Sidebar } from "./Sidebar";
 export { default as InnerSidebar } from "./InnerSidebar";
 export { default as Navbar } from "./Navbar";
 export { default as Logo } from "./Logo";
+export { default as FlowRow} from './FormRow'
