65dd0ad9031fd475d6d8d1f608bd8558a5ca41d9
Rehaan9khan <rehaan.khan@stud.hochschule-heidelberg.de>
2024-01-25 23:58:25 +0530

    Implemented JWT & Logic

--- USER-API-BETA/1/routes/authRoutes.js
+++ USER-API-BETA/1/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
 const router = Router();
 import {login, register} from '../controllers/authControllers.js'
+import { validateRegisterInput } from '../middleware/validationMiddleware.js';
 
-router.post('/register', register);
+router.post('/register',validateRegisterInput, register);
 router.post('/login', login);
 
 export default router;
